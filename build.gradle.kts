8af52542ccef20aa0356dd33f0e970be9ecc6f4f
Elias Ferreira <eliasmflilico@gmail.com>
2021-12-16 01:13:55 -0300

    Add gralde dsl plugin compiler

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("org.springframework.boot") version "2.6.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.6.0"
+    kotlin("plugin.spring") version "1.6.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.passay:passay:1.6.1")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
