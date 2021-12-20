d90b16646858be1225c5ac07f141b83c70efccd3
Elias Ferreira <eliasmflilico@gmail.com>
2021-12-19 21:54:34 -0300

    Improved security initialization

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,14 @@ repositories {
     mavenCentral()
 }
 
+group = "br.com.softwareplace"
+version = "1.0.0"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
