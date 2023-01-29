7e2c044adf9f8afca2d0167689ccd6a4828a23d1
Elias Meireles <eliasmflilico@gmail.com>
2023-01-29 12:11:39 -0300

    Removed unused plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,4 +11,3 @@ pluginManagement {
 
 includeBuild("spring-boot-included-builds/build-configuration")
 includeBuild("spring-boot-included-builds/build-source")
-includeBuild("spring-boot-included-builds/spring-openapi")
