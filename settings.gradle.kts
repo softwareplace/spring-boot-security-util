1d442736af8a149856b82ec14223cadade4ea4cf
Elias Meireles <eliasmflilico@gmail.com>
2023-01-28 21:18:10 -0300

    Add gradle files

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "spring-boot-security-util"
+include(":base")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+    }
+}
+
+includeBuild("spring-boot-included-builds/build-configuration")
+includeBuild("spring-boot-included-builds/build-source")
+includeBuild("spring-boot-included-builds/spring-openapi")
