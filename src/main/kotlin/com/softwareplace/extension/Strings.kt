a5d56e292f3bc109a390090cb864502a39bd9a58
Elias Meireles <eliasmflilico@gmail.com>
2023-02-09 10:58:22 -0300

    Fixed deprecated

--- src/main/kotlin/com/softwareplace/extension/Strings.kt
+++ src/main/kotlin/com/softwareplace/extension/Strings.kt
@@ -1,7 +1,9 @@
 package com.softwareplace.extension
 
+import java.util.*
+
 
 fun String.addAtStartAsCamelCase(value: String): String {
-    val firstLetter = substring(0, 1).toUpperCase()
+    val firstLetter = substring(0, 1).uppercase(Locale.getDefault())
     return "$value$firstLetter${substring(1, length)}"
 }
