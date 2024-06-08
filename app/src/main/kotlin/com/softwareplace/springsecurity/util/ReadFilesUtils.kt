c8b85042cb0220bdd1a45f842f066a8fe22997ab
Elias Meireles <eliasmflilico@gmail.com>
2024-06-08 13:15:32 -0300

    fix: removed reading filed logs.

--- app/src/main/kotlin/com/softwareplace/springsecurity/util/ReadFilesUtils.kt
+++ app/src/main/kotlin/com/softwareplace/springsecurity/util/ReadFilesUtils.kt
@@ -1,6 +1,5 @@
 package com.softwareplace.springsecurity.util
 
-import com.softwareplace.jsonlogger.log.kLogger
 import org.springframework.core.io.ResourceLoader
 import java.io.File
 import java.nio.file.Files
@@ -8,10 +7,9 @@ import java.nio.file.Files
 class ReadFilesUtils {
     companion object {
         fun readFileBytes(resourceLoader: ResourceLoader, filePath: String): ByteArray {
-            kLogger.info("Reading content from file: $filePath")
             val resource = resourceLoader.getResource(filePath)
 
-            val fileContent = if (resource.exists()) {
+            return if (resource.exists()) {
                 resource.inputStream.use { it.readBytes() }
             } else {
                 val file = File(filePath)
@@ -20,9 +18,6 @@ class ReadFilesUtils {
                 }
                 throw IllegalArgumentException("File not found: $filePath")
             }
-
-            kLogger.info("Content read from file: ${fileContent.size} bytes")
-            return fileContent
         }
     }
 }
